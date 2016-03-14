4e20e52bdb5f54145a0d03d8d4f8b031212beb3f
François Monniot <francoismonniot@gmail.com>
2016-03-13 20:36:40 -0700

    Fix typo in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 test "${TRAVIS_PULL_REQUEST}" == "false" && \
     test "${TRAVIS_JDK_VERSION}" == "oraclejdk7" && \
     test "${TRAVIS_TAG}" != "" && \
-    echo deploying && \
+    echo Deploying to Bintray && \
     mvn deploy --settings settings.xml
