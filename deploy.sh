27f690df177b4def63ac2b9074d2527d36610ca6
François Monniot <francoismonniot@gmail.com>
2016-03-15 20:36:14 -0700

    Use the correct profile when deploying

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ test "${TRAVIS_PULL_REQUEST}" == "false" && \
     test "${TRAVIS_JDK_VERSION}" == "oraclejdk7" && \
     test "${TRAVIS_TAG}" != "" && \
     echo Deploying to Bintray && \
-    mvn deploy --settings settings.xml
+    mvn -Prelease deploy --settings settings.xml
