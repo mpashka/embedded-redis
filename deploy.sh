d631d394a333703a3df44193729f4ac7ca0555ee
François Monniot <f.monniot@samsung.com>
2017-05-16 10:55:50 +0200

    #6 Use gradle instead of maven

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ test "${TRAVIS_PULL_REQUEST}" == "false" && \
     test "${TRAVIS_JDK_VERSION}" == "oraclejdk7" && \
     test "${TRAVIS_TAG}" != "" && \
     echo Deploying to Bintray && \
-    mvn -Prelease deploy --settings settings.xml
+    ./gradlew bintrayUpload
